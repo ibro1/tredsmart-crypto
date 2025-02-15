34bda9d17ec983e0c78dcab5121451970f1f9e2e
Abubakar <abubakaribro1@gmail.com>
2025-02-15 09:50:48 +0100

    fix buffer

--- app/utils/buffer-polyfill.ts
+++ app/utils/buffer-polyfill.ts
@@ -1,5 +1,11 @@
 import { Buffer } from 'buffer'
 
-if (typeof window !== 'undefined') {
-  window.Buffer = Buffer
+declare global {
+  interface Window {
+    Buffer: typeof Buffer;
+  }
+}
+
+if (typeof window !== 'undefined' && !window.Buffer) {
+  window.Buffer = Buffer || require('buffer').Buffer;
 }
